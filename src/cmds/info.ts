2778c66a8a6ac460ab4ce2efe64ccf25a3036e1b
Max Koon <theakman96@gmail.com>
2020-10-10 16:30:35 -0400

    parses service.yaml file

--- src/cmds/info.ts
+++ src/cmds/info.ts
@@ -11,10 +11,16 @@ export default (program: CommanderStatic) => {
         .action(() => {
             console.log(chalk.bold('Nix2 CLI version ' + VERSION));
             // get the service
-            let service = getServiceContext();
-            if (service == null) {
-                console.log(chalk.gray('No service in this directory'));
-                return;
+            try {
+                let service = getServiceContext();
+                console.log(service);
+                
+                if (service == null) {
+                    console.log(chalk.gray('No service in this directory'));
+                    return;
+                }
+            } catch (err) {
+                console.error(chalk.red(`ERR: ${err.message}`));
             }
 
         });
--- src/service.ts
+++ src/service.ts
@@ -1,17 +1,35 @@
 import { SERVICE_FILE_NAME } from "./constants";
 const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
 
+// check if the file exists
+const serviceFileExists = (serviceFilePath: string): boolean => {
+    try { return fs.existsSync(serviceFilePath); } 
+    catch(err) { throw new Error("Could not check if file exists"); }
+}
 
-export const getServiceContext = () => {
-    let path = process.cwd() + "/" + SERVICE_FILE_NAME;
+// get the file from the path
+const getServiceFile = (serviceFilePath: string): File => {
+    try { return fs.readFileSync(serviceFilePath, 'utf-8'); }
+    catch (err) { throw new Error("Could not read service.yaml"); }
+}
+
+// returns a js object from the yaml
+const getServiceObject = (file: File): object => {
     try {
-        if (fs.existsSync(path)) {
-            return true;
-        } else {
-            return null;
-        }
-    } catch(err) {
-        console.error(err);
-        return null;
-    }
+        // parse the file's yaml
+        const serviceObject = yaml.load(file);
+        // throw an error if the response is anything but an object
+        if (typeof serviceObject != "object") throw new Error("Could not parse yaml");
+        return serviceObject;
+    } catch (err) { throw new Error("Could not parse yaml"); }
+}
+
+export const getServiceContext = (): object|null => {
+    const serviceFilePath = path.join(process.cwd(), SERVICE_FILE_NAME);
+    if (!serviceFileExists(serviceFilePath)) return null;
+    const file = getServiceFile(serviceFilePath);
+    return getServiceObject(file);           
+    
 }
