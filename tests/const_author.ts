4fbe9566f3f3f8f59b355a692121cb342e47f48f
Max Koon <theakman96@gmail.com>
2020-11-14 00:47:31 -0500

    removed core tests

--- tests/const_author.ts
+++
@@ -1,8 +0,0 @@
-export const WORKING_AUTHOR_1 = {
-    email: 'me@example.com',
-    name: 'Mock Author 1',
-    publicEmail: 'author1@example.com',
-    url: 'auth1.nix2.io',
-    alert: '*',
-    flags: ['leadDev'],
-};
--- tests/const_response.ts
+++
@@ -1,9 +0,0 @@
-import { ResponseType } from '../src/types';
-
-export const WORKING_RESPONSE_1: ResponseType = {
-    description: 'Success',
-    returnType: null,
-    errorMessage: null,
-};
-
-export const WORKING_RESPONSES = [WORKING_RESPONSE_1];
